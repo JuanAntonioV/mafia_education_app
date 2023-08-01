e62a289a0f37bcf44c532e375e1df1fae432e2fa
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-08-01 22:57:59 +0700

    update: init the getx package

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:mafia_education_app/app/routes/app_pages.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
+  void _handleOnClick() {
+    Get.toNamed(Routes.WELCOME);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'HomeView is working',
+            style: TextStyle(fontSize: 20),
+          ),
+          ElevatedButton(
+            onPressed: _handleOnClick,
+            child: const Text('Go to WelcomeView'),
+          )
+        ],
+      )),
+    );
+  }
+}
